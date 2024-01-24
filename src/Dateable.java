936e9f1aa1ed6c00879a75d1a10f992427082f84
charlottec2026 <charlottec2026@student.cis.edu.hk>
2024-01-24 08:54:21 +0800

    code written

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("PLease enter your age as a whole number: ");
+        String age = scanner.nextLine();
+        int ageAsNum = Integer.parseInt(age);
+        int largestAgeDateable = (ageAsNum / 2)+7;
+        System.out.println(ageAsNum + "-year-olds should date someone who is at least " + largestAgeDateable + " years old.");
+
     }
 }
