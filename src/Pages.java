ccaa7972d0f4a53e309dab1cf8612619b2b0c0be
charlottec2026 <charlottec2026@student.cis.edu.hk>
2024-01-25 17:03:25 +0800

    pages finished

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input your age as a whole number: ");
+        String age = scanner.nextLine();
+        int ageAsNum = Integer.parseInt(age);
+        if (ageAsNum < 0) {
+            System.out.println("Please input a valid number, then try again.");
+        }
+        if (ageAsNum > 0) {
+            int howManyPages = 100-ageAsNum;
+            System.out.println(ageAsNum + "-year-olds should read at least " + howManyPages + " pages before giving up on a book.");
 
+        }
     }
 }
